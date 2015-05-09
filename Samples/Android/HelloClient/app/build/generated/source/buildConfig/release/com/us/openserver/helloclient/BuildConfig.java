c25ef6913e880eef189f42c656c6ccd55c3091af
mjanulaitis <mike@uppersetting.com>
2015-05-09 16:57:33 -0600

    Added copyright notice to source files

---
+++ Samples/Android/HelloClient/app/build/generated/source/buildConfig/release/com/us/openserver/helloclient/BuildConfig.java
@@ -0,0 +1,32 @@
+/*
+Copyright 2015 Upper Setting Corporation
+
+This file is part of DotNetOpenServer SDK.
+
+DotNetOpenServer SDK is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+DotNetOpenServer SDK is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+DotNetOpenServer SDK. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.us.openserver.helloclient;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.us.openserver.helloclient";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
