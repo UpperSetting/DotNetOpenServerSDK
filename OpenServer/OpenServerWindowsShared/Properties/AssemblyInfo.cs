bb286bd66f37f9a9d8a40eb135e3fe396a9d9348
mjanulaitis <mike@uppersetting.com>
2015-05-09 17:22:11 -0600

    Updated assembly information

--- OpenServer/OpenServerWindowsShared/Properties/AssemblyInfo.cs
+++ OpenServer/OpenServerWindowsShared/Properties/AssemblyInfo.cs
@@ -18,39 +18,13 @@ DotNetOpenServer SDK. If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("OpenServerSharedWindows")]
+[assembly: AssemblyTitle("OpenServerWindowsShared")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("OpenServerSharedWindows")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: log4net.Config.XmlConfigurator()]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ebef5ab1-bfeb-4d22-afa6-9f3a9d956b60")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyCompany("Upper Setting Corporation")]
+[assembly: AssemblyProduct("DotNetOpenServer SDK")]
+[assembly: AssemblyCopyright("Copyright © 2015 Upper Setting Corporation")]
+[assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Protocols/Hello/Windows/HelloProtocolClient/Properties/AssemblyInfo.cs
+++ Protocols/Hello/Windows/HelloProtocolClient/Properties/AssemblyInfo.cs
@@ -17,33 +17,14 @@ You should have received a copy of the GNU General Public License along with
 DotNetOpenServer SDK. If not, see <http://www.gnu.org/licenses/>.
 */
 
-using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("HelloProtocolClient")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("HelloProtocolClient")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Upper Setting Corporation")]
+[assembly: AssemblyProduct("DotNetOpenServer SDK")]
+[assembly: AssemblyCopyright("Copyright © 2015 Upper Setting Corporation")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
