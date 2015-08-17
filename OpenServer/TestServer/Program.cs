cad861c427332017d19fcc54d6da9f7d152fbced
mjanulaitis <mike@uppersetting.com>
2015-08-17 16:00:08 -0600

    Added references so the program can programatically load the protocols

--- OpenServer/TestServer/Program.cs
+++ OpenServer/TestServer/Program.cs
@@ -22,9 +22,9 @@ using System.Collections.Generic;
 using US.OpenServer;
 using US.OpenServer.Configuration;
 using US.OpenServer.Protocols;
-//using US.OpenServer.Protocols.Hello;
-//using US.OpenServer.Protocols.KeepAlive;
-//using US.OpenServer.Protocols.WinAuth;
+using US.OpenServer.Protocols.Hello;
+using US.OpenServer.Protocols.KeepAlive;
+using US.OpenServer.Protocols.WinAuth;
 
 namespace TestServer
 {
@@ -35,37 +35,33 @@ namespace TestServer
             Server server = null;
             try
             {
-                #region Load configuration from the app.config
+#if USE_APP_CONFIG
                 server = new Server();
-                #endregion
+#else                
+                ServerConfiguration cfg = new ServerConfiguration();
+                cfg.Host = "0.0.0.0";//bind address
+                cfg.TlsConfiguration.Enabled = false;                
 
-                #region Configure programatically
-                //Logger logger = new Log4NetLogger("DotNetOpenServer");
-                //logger.LogPackets = true;
+                Dictionary<ushort, ProtocolConfiguration> protocolConfigurations =
+                    new Dictionary<ushort, ProtocolConfiguration>();
 
-                //ServerConfiguration cfg = new ServerConfiguration();
+                protocolConfigurations.Add(KeepAliveProtocol.PROTOCOL_IDENTIFIER,
+                    new ProtocolConfiguration(KeepAliveProtocol.PROTOCOL_IDENTIFIER, typeof(KeepAliveProtocol)));
 
-                //Dictionary<ushort, ProtocolConfiguration> protocolConfigurations =
-                //    new Dictionary<ushort, ProtocolConfiguration>();
+                WinAuthProtocolConfigurationServer winAuthCfg =
+                    new WinAuthProtocolConfigurationServer(
+                        WinAuthProtocol.PROTOCOL_IDENTIFIER,
+                        typeof(WinAuthProtocolServer));
+                winAuthCfg.AddRole("Administrators");
+                winAuthCfg.AddUser("TestUser");
+                protocolConfigurations.Add(WinAuthProtocol.PROTOCOL_IDENTIFIER, winAuthCfg);
 
-                //protocolConfigurations.Add(KeepAliveProtocol.PROTOCOL_IDENTIFIER,
-                //    new ProtocolConfiguration(KeepAliveProtocol.PROTOCOL_IDENTIFIER, typeof(KeepAliveProtocol)));
+                protocolConfigurations.Add(HelloProtocol.PROTOCOL_IDENTIFIER,
+                    new ProtocolConfiguration(HelloProtocol.PROTOCOL_IDENTIFIER, typeof(HelloProtocolServer)));
 
-                //WinAuthProtocolConfigurationServer winAuthCfg =
-                //    new WinAuthProtocolConfigurationServer(
-                //        WinAuthProtocol.PROTOCOL_IDENTIFIER,
-                //        typeof(WinAuthProtocolServer));
-                //winAuthCfg.AddRole("Administrators");
-                //winAuthCfg.AddUser("TestUser");
-                //protocolConfigurations.Add(WinAuthProtocol.PROTOCOL_IDENTIFIER, winAuthCfg);
-
-                //protocolConfigurations.Add(HelloProtocol.PROTOCOL_IDENTIFIER,
-                //    new ProtocolConfiguration(HelloProtocol.PROTOCOL_IDENTIFIER, typeof(HelloProtocolServer)));
-
-                //Server server = new Server(logger, cfg, protocolConfigurations);
-                #endregion
-
-                server.Logger.Log(Level.Info, "Press any key to quit.");                
+                server = new Server(cfg, protocolConfigurations);
+#endif
+                server.Logger.Log(Level.Info, "Press any key to quit.");  
             }
             catch (Exception ex)
             {
