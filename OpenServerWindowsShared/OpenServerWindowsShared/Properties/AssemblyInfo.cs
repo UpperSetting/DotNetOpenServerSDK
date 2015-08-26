ae7e9925fe8eac4d5d0ecb4aad53ebdc4d562095
mjanulaitis <mike@uppersetting.com>
2015-08-26 07:10:46 -0600

    Moved

---
+++ OpenServerWindowsShared/OpenServerWindowsShared/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+/*
+Copyright 2015 Upper Setting Corporation
+
+This file is part of DotNetOpenServer SDK.
+
+DotNetOpenServer SDK is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+DotNetOpenServer SDK is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+DotNetOpenServer SDK. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("OpenServerWindowsShared")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Upper Setting Corporation")]
+[assembly: AssemblyProduct("DotNetOpenServer SDK")]
+[assembly: AssemblyCopyright("Copyright © 2015 Upper Setting Corporation")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: log4net.Config.XmlConfigurator()]
