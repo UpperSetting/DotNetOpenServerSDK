30de705fa6cb57bb47c7e580d325dd4b308b3391
mjanulaitis <mike@uppersetting.com>
2016-01-07 11:10:08 -0700

    Initial Commit

---
+++ Protocols/DatabaseAuth/Java/src/com/us/openserver/protocols/databaseauth/DatabaseAuthProtocol.java
@@ -0,0 +1,37 @@
+/*
+Copyright 2015 Upper Setting Corporation
+
+This file is part of DotNetOpenServer SDK.
+
+DotNetOpenServer SDK is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+DotNetOpenServer SDK is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+DotNetOpenServer SDK. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.us.openserver.protocols.databaseauth;
+
+import com.us.openserver.Level;
+import com.us.openserver.protocols.AuthenticationProtocolBase;
+
+public class DatabaseAuthProtocol extends AuthenticationProtocolBase
+{
+    public static final int PROTOCOL_IDENTIFIER = 0x0002;
+    
+    protected DatabaseAuthProtocol()
+    {
+    }
+    
+    protected void log(Level level, String message)
+    {
+        session.log(level, String.format("[DatabaseAuth] %1$s", message));
+    }
+}
---
+++ Protocols/DatabaseAuth/Java/src/com/us/openserver/protocols/databaseauth/DatabaseAuthProtocolClient.java
@@ -0,0 +1,110 @@
+/*
+Copyright 2015 Upper Setting Corporation
+
+This file is part of DotNetOpenServer SDK.
+
+DotNetOpenServer SDK is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+DotNetOpenServer SDK is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+DotNetOpenServer SDK. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.us.openserver.protocols.databaseauth;
+
+import com.us.openserver.*;
+import com.us.openserver.protocols.*;
+
+import java.io.IOException;
+
+public class DatabaseAuthProtocolClient extends DatabaseAuthProtocol
+{
+    public static final int TIMEOUT = 120000;
+    
+    public DatabaseAuthProtocolClient()
+    {
+    }
+
+    public boolean authenticate(String userName, String password)
+    {
+        synchronized (this)
+        {
+            if (session == null)
+                return false;
+
+            super.userName = userName;
+            session.UserName = userName;
+
+            BinaryWriter bw = new BinaryWriter();
+            try
+            {
+                bw.writeUInt16(DatabaseAuthProtocolClient.PROTOCOL_IDENTIFIER);
+                bw.write((byte) DatabaseAuthProtocolCommands.AUTHENTICATE);
+                bw.writeString(userName);
+                bw.writeString(password);
+    
+                PacketWriter pw = new PacketWriter(session, bw.toByteArray());
+                pw.execute();
+            }
+            finally { try { bw.close(); } catch (IOException ex) { } }
+
+            try { wait(DatabaseAuthProtocolClient.TIMEOUT); }
+            catch (InterruptedException ex) {  }
+
+            return isAuthenticated;
+        }
+    }
+
+    public void onPacketReceived(BinaryReader br)
+    {
+        synchronized (this)
+        {
+            if (session == null)
+                return;
+
+            int command = br.readByte();
+            switch (command)
+            {
+                case DatabaseAuthProtocolCommands.AUTHENTICATED:
+                	userId = br.readInt32();
+                    isAuthenticated = true;
+                    session.IsAuthenticated = true;
+                    log(Level.Info, "Authenticated.");
+                    notifyAll();
+                    break;
+
+                case DatabaseAuthProtocolCommands.ACCESS_DENIED:
+                    log(Level.Notice, "Access denied.");
+                    notifyAll();
+                    break;
+
+                case DatabaseAuthProtocolCommands.ERROR:
+                {
+                    try
+                    {
+                        String errorMessage = br.readString();
+                        log(Level.Notice, errorMessage);
+                    }
+                    catch (IOException ex)
+                    {
+                    }
+                    notifyAll();
+                    break;
+                }
+                
+                default:
+                    log(Level.Error, String.format("Invalid or unsupported command.  Command: %1$s", command));
+                    break;
+            }
+        }
+    }
+}
+
+
