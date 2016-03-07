6f7944ef5980dd739e1b389006bb268977584308
mjanulaitis <mike@uppersetting.com>
2016-03-07 02:12:28 -0700

    Added error message to exception

--- Protocols/DatabaseAuth/Windows/DatabaseAuthProtocolShared/DatabaseAuthProtocolException.cs
+++ Protocols/DatabaseAuth/Windows/DatabaseAuthProtocolShared/DatabaseAuthProtocolException.cs
@@ -11,5 +11,24 @@ namespace US.OpenServer.Protocols.DatabaseAuth
         {
             ErrorCode = errorCode;
         }
+
+        public override string Message
+        {
+            get
+            {
+                switch (ErrorCode)
+                {
+                    case DatabaseAuthProtocolCommands.ACCESS_DENIED:
+                        return DatabaseAuthProtocol.ERROR_ACCESS_DENIED;
+
+                    case DatabaseAuthProtocolCommands.ACCESS_DENIED_EMAIL_NOT_VERIFIED:
+                        return DatabaseAuthProtocol.ERROR_NOT_VERIFIED;
+
+                    case DatabaseAuthProtocolCommands.ACCESS_DENIED_INVALID_PASSWORD:
+                        return DatabaseAuthProtocol.ERROR_USER_NOT_FOUND;
+                }
+                return base.Message;
+            }
+        }
     }
 }
