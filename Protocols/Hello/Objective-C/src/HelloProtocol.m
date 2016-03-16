2209b59471061d97f64d96eb3fea0e9d630b2c27
Michael Janulaitis <mjanulaitis@Michaels-iMac.local>
2016-03-16 17:12:23 -0600

    Build 38

--- Protocols/Hello/Objective-C/src/HelloProtocol.m
+++ Protocols/Hello/Objective-C/src/HelloProtocol.m
@@ -3,7 +3,6 @@
 //  source: ./com/us/openserver/protocols/hello/HelloProtocol.java
 //
 
-
 #include "HelloProtocol.h"
 #include "IOSObjectArray.h"
 #include "J2ObjC_source.h"
@@ -13,23 +12,25 @@
 
 @implementation ComUsOpenserverProtocolsHelloHelloProtocol
 
+J2OBJC_IGNORE_DESIGNATED_BEGIN
 - (instancetype)init {
   ComUsOpenserverProtocolsHelloHelloProtocol_init(self);
   return self;
 }
+J2OBJC_IGNORE_DESIGNATED_END
 
-- (void)logWithComUsOpenserverLevelEnum:(ComUsOpenserverLevelEnum *)level
-                           withNSString:(NSString *)message {
-  [((ComUsOpenserverSessionSession *) nil_chk(session_)) logWithComUsOpenserverLevelEnum:level withNSString:NSString_formatWithNSString_withNSObjectArray_(@"[Hello] %1$s", [IOSObjectArray newArrayWithObjects:(id[]){ message } count:1 type:NSObject_class_()])];
+- (void)logWithComUsOpenserverLevel:(ComUsOpenserverLevel *)level
+                       withNSString:(NSString *)message {
+  [((ComUsOpenserverSessionSession *) nil_chk(session_)) logWithComUsOpenserverLevel:level withNSString:NSString_formatWithNSString_withNSObjectArray_(@"[Hello] %s", [IOSObjectArray newArrayWithObjects:(id[]){ message } count:1 type:NSObject_class_()])];
 }
 
 + (const J2ObjcClassInfo *)__metadata {
   static const J2ObjcMethodInfo methods[] = {
     { "init", "HelloProtocol", NULL, 0x4, NULL, NULL },
-    { "logWithComUsOpenserverLevelEnum:withNSString:", "log", "V", 0x4, NULL, NULL },
+    { "logWithComUsOpenserverLevel:withNSString:", "log", "V", 0x4, NULL, NULL },
   };
   static const J2ObjcFieldInfo fields[] = {
-    { "PROTOCOL_IDENTIFIER_", NULL, 0x19, "I", NULL, NULL, .constantValue.asInt = ComUsOpenserverProtocolsHelloHelloProtocol_PROTOCOL_IDENTIFIER },
+    { "PROTOCOL_IDENTIFIER", "PROTOCOL_IDENTIFIER", 0x19, "I", NULL, NULL, .constantValue.asInt = ComUsOpenserverProtocolsHelloHelloProtocol_PROTOCOL_IDENTIFIER },
   };
   static const J2ObjcClassInfo _ComUsOpenserverProtocolsHelloHelloProtocol = { 2, "HelloProtocol", "com.us.openserver.protocols.hello", NULL, 0x1, 2, methods, 1, fields, 0, NULL, 0, NULL, NULL, NULL };
   return &_ComUsOpenserverProtocolsHelloHelloProtocol;
@@ -38,13 +39,17 @@
 @end
 
 void ComUsOpenserverProtocolsHelloHelloProtocol_init(ComUsOpenserverProtocolsHelloHelloProtocol *self) {
-  (void) ComUsOpenserverProtocolsProtocolBase_init(self);
+  ComUsOpenserverProtocolsProtocolBase_init(self);
 }
 
 ComUsOpenserverProtocolsHelloHelloProtocol *new_ComUsOpenserverProtocolsHelloHelloProtocol_init() {
   ComUsOpenserverProtocolsHelloHelloProtocol *self = [ComUsOpenserverProtocolsHelloHelloProtocol alloc];
   ComUsOpenserverProtocolsHelloHelloProtocol_init(self);
   return self;
+}
+
+ComUsOpenserverProtocolsHelloHelloProtocol *create_ComUsOpenserverProtocolsHelloHelloProtocol_init() {
+  return new_ComUsOpenserverProtocolsHelloHelloProtocol_init();
 }
 
 J2OBJC_CLASS_TYPE_LITERAL_SOURCE(ComUsOpenserverProtocolsHelloHelloProtocol)
